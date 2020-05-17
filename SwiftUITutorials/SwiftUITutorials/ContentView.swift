9dd29cbbaad390d9b5be2280fd3c2efc2264ba45
Ferlix Yanto Wang <ferlix.wang@tokopedia.com>
2020-05-17 18:24:41 +0700

    initial commit with projects created

---
+++ SwiftUITutorials/SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by Ferlix Yanto Wang on 17/05/20.
+//  Copyright © 2020 Ferlix Yanto Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
